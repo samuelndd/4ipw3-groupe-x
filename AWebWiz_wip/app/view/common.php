c8cf4718e995ecee18306fd93a774e1a6cbed9f1
samuelnd <samuelndala13@gmail.com>
2024-12-28 05:33:48 +0100

    projet prof

--- AWebWiz_wip/app/view/common.php
+++ AWebWiz_wip/app/view/common.php
@@ -7,16 +7,18 @@ function html_head($menu_a=[], $user_id="", $user_role="")
 	ob_start();
     // et puis du html
 	?>
-	<html lang="fr">
-	<head>
-		<title>AWebWiz Template (MVC)</title>
-        <link rel="stylesheet" href="./pubic/css/bootstrap/bootstrap.min.css" />  <!-- lib externe -->
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-        <link rel="stylesheet" href="./pubic/css/internal/main.css" /> <!-- lib interne / perso -->
-
-	</head>
+    <html lang="fr">
+    <head>
+        <title>AWebWiz Template (MVC)</title>
+        <link rel="stylesheet" href="./css/bootstrap/bootstrap.min.css" />  <!-- lib externe -->
+        <link rel="stylesheet" href="./css/internal/main.css" /> <!-- lib interne / perso -->
+        <script
+                src="https://code.jquery.com/jquery-3.4.1.js"
+                integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
+                crossorigin="anonymous"></script>
+        <script src="./js/quirks/QuirksMode.js"></script>
+        <script src="./js/internal/favorite.js"></script>
+    </head>
 	<body>
     <header>
         <h1>
